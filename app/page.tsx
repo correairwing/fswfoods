6c928f7d5293ff3629e1556acbb5c27975f38f41
Irwing <irwingcorrea95@gmail.com>
2024-05-03 17:15:42 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -11,6 +12,18 @@ const Home = () => {
       </div>
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src="/logo-banner.png"
+          alt="Até 30% de descontos em pizzas"
+          height={0}
+          width={0}
+          sizes="100vw"
+          quality={100}
+          className="h-auto w-full object-contain"
+        />
       </div>
     </>
   );
